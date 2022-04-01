f1d682ae088c0ec403fec43faa3a0ce623eae4db
micheaol <micheaol80@gmail.com>
2022-04-01 15:14:21 +0100

    Display doctor's details

--- src/pages/Doctors/DoctorCard.jsx
+++ src/pages/Doctors/DoctorCard.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-quotes */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable react/jsx-no-comment-textnodes */
 /* eslint-disable react/forbid-prop-types */
 import React from 'react';
 import PropTypes from 'prop-types';
@@ -5,7 +8,18 @@ import PropTypes from 'prop-types';
 function DoctorCard({ doctor }) {
   return (
     <>
-      <div>{doctor.map((doc) => <div key={doc.id} className='"max-w-sm rounded overflow-hidden shadow-lg'>{doc.name}</div>)}</div>
+      <div>
+        {doctor.map((doc) => (
+          <div
+            key={doc.id}
+            className='"max-w-sm rounded overflow-hidden shadow-lg'
+          >
+            <h1>{doc.name}</h1>
+            <h1>experience: {doc.experience}</h1>
+            <img src={doc.profile_pic} alt='doctor' />
+          </div>
+        ))}
+      </div>
     </>
   );
 }
