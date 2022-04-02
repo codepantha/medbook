45fc71f15c19a2cdc2aede2391648bff6d96744a
Amr Ahmed <amr_ahmed65@hotmail.com>
2022-04-02 22:52:32 +0200

    remove padding from routes container

--- src/components/DetailPage/DetailPage.jsx
+++ src/components/DetailPage/DetailPage.jsx
@@ -27,19 +27,38 @@ const DetailPage = () => (
           </li>
         </ul>
         <p>
-          Meredith Grey is the head of general surgery and former Director of the Residency
-          Program at Grey Sloan Memorial Hospital. She is the daughter of the now-deceased Ellis
-          Grey, a famed general surgeon, and the now-deceased Thatcher Grey.
+          Meredith Grey is the head of general surgery and former Director of the Residency Program
+          at Grey Sloan Memorial Hospital. She is the daughter of the now-deceased Ellis Grey, a
+          famed general surgeon, and the now-deceased Thatcher Grey.
         </p>
         <div className="flex justify-between">
-          <button type="button" className="bg-primary hover:bg-lime-400 p-4 rounded-full text-slate-50">Appointment</button>
-          <button type="button" className="bg-red-600 hover:bg-red-400 p-4 rounded-full text-slate-50">Delete</button>
+          <button
+            type="button"
+            className="bg-primary hover:bg-lime-400 p-4 rounded-full text-slate-50"
+          >
+            Appointment
+          </button>
+          <button
+            type="button"
+            className="bg-red-600 hover:bg-red-400 p-4 rounded-full text-slate-50"
+          >
+            Delete
+          </button>
         </div>
       </aside>
     </main>
     <footer>
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-12 bg-primary hover:bg-lime-400 font-bold text-3xl text-slate-50 pl-10 p-2  pr-3 rounded-r-full cursor-pointer" viewBox="0 0 20 20" fill="white">
-        <path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd" />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-12 bg-primary hover:bg-lime-400 font-bold text-3xl text-slate-50 pl-10 p-2  pr-3 rounded-r-full cursor-pointer"
+        viewBox="0 0 20 20"
+        fill="white"
+      >
+        <path
+          fillRule="evenodd"
+          d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+          clipRule="evenodd"
+        />
       </svg>
     </footer>
   </>
